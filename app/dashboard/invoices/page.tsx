e5d795144984e581ee44b41c752b4bc8fc7b884b
nyafologus <nyafologus@gmail.com>
2023-11-12 02:51:31 +0100

    Construct invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import InvoicesTable from "@/app/ui/invoices/table";
+import Search from "@/app/ui/search";
+import React, { Suspense } from "react";
 
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
   return (
     <>
-      <p>Invoices page</p>
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1 className={` ${lusitana.className} text-2xl`}>📜 Invoices 📜</h1>
+        </div>
+
+        <div className="mt-4 flex items-center gap-2 md:mt-8">
+          <Search placeholder="Search invoices..." />
+          <CreateInvoice />
+        </div>
+
+        <Suspense>
+          <InvoicesTable
+            query={query}
+            currentPage={currentPage}
+          ></InvoicesTable>
+        </Suspense>
+
+        <div className="mt-5">
+          {/* <Pagination totalPages={totalPages} /> */}
+        </div>
+      </div>
     </>
   );
 }
