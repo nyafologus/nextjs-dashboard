4d728780257df82cf24a03146cc861471acdff40
nyafologus <nyafologus@gmail.com>
2023-11-14 03:47:07 +0100

    Use zod to validate data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,18 @@
 "use server";
 
+// import Zod and define a schema that matches the shape of your form object
+import { z } from "zod";
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
     customerId: formData.get("customerId"),
@@ -8,4 +21,7 @@ export async function createInvoice(formData: FormData) {
   };
   // Test it out:
   console.log(rawFormData);
+
+  console.log(typeof rawFormData.amount);
+  // You'll notice that amount is of type string and not number. This is because input elements with type="number" actually return a string, not a number!
 }
