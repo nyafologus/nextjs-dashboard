2cfb259f1bc656f59ebf2a652abd47670ad1c257
nyafologus <nyafologus@gmail.com>
2023-11-15 03:59:37 +0100

    Insert data into DB

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { sql } from "@vercel/postgres";
 // import Zod and define a schema that matches the shape of your form object
 import { z } from "zod";
 
@@ -22,6 +23,12 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
   // Test it out:
   // console.log(rawFormData);
 
