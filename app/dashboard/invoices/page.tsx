6f69b733283672d3006082c7cf02a23381656a67
nyafologus <nyafologus@gmail.com>
2023-11-19 21:15:57 +0100

    Incorporate wink font with ornaments

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,13 @@ import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import React, { Suspense } from "react";
 
+import localFont from "next/font/local";
+
+const winkFont = localFont({
+  src: "../../../public/wink-script-regular.woff2",
+  display: "swap",
+});
+
 export default async function Page({
   searchParams,
 }: {
@@ -24,7 +31,13 @@ export default async function Page({
     <>
       <div className="w-full">
         <div className="flex w-full items-center justify-between">
-          <h1 className={` ${lusitana.className} text-2xl`}>📜 Invoices 📜</h1>
+          {/* <h1 className={` ${lusitana.className} text-2xl`}>📜 Invoices 📜</h1> */}
+
+          <h1 className={`${winkFont.className}  text-6xl`}>
+            📜
+            <span className="swooshed">A</span>mazing Invoice
+            <span className="ss06">s</span>📜
+          </h1>
         </div>
 
         <div className="mt-4 flex items-center gap-2 md:mt-8">
